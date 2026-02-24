f240bac0210216e17df16ccfdefc37eb1ebcd6df
RodaMrtnz <42496566@comunidadort.edu.ar>
2026-02-24 18:31:36 -0300

    Setup clean architecture solution

---
+++ GymCore.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using GymCore.Domain.Enums;
+
+namespace GymCore.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ GymCore.Domain/Enums/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace GymCore.Domain.Enums;
+
+public enum UserRole
+{
+    User = 1,
+    Trainer = 2,
+    Staff = 3,
+    Admin = 4
+}
